86213db3369ce4ee62698c97c3da1c7b9ef33ecd
swatijadhav <swati.nakshtra17@gmail.com>
2022-01-14 14:45:30 +0530

    display user-success notice using turbo_stream in healpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+  NOTICE = {
+    create: 'User created successfully',
+    update: 'User updated successfully',
+    delete: 'User deleted successfully',
+    error: 'Something went wrong'
+  }.freeze
+
+  def notice_stream(message:, status:)
+    turbo_stream.replace 'notice',
+      partial: 'notice',
+      locals: { notice: NOTICE[message], status: status }
+  end
 end
