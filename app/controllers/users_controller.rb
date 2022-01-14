8002db4ca0a0234fe40b9c517e84edf9a53a0675
swatijadhav <swati.nakshtra17@gmail.com>
2022-01-14 14:37:35 +0530

    render new-user with format.turbo_stream

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,9 +25,11 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        format.turbo_stream # add format turbo_stream
         format.html { redirect_to user_url(@user), notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
+        format.turbo_stream # add format turbo_stream
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
