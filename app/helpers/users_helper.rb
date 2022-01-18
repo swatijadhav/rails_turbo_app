ffffc13adce46beb5d6f0a4671f959319fdc1c23
swatijadhav <swati.nakshtra17@gmail.com>
2022-01-18 11:54:35 +0530

    handle user creation error

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,12 +3,16 @@ module UsersHelper
     create: 'User created successfully',
     update: 'User updated successfully',
     delete: 'User deleted successfully',
-    error: 'Something went wrong'
+    error: 'Something went wrong. Please try again.'
   }.freeze
 
   def notice_stream(message:, status:)
     turbo_stream.replace 'notice',
       partial: 'notice',
       locals: { notice: NOTICE[message], status: status }
+  end
+
+  def form_user_stream(user:)
+    turbo_stream.replace 'form', partial: 'form', locals: { user: user }
   end
 end
